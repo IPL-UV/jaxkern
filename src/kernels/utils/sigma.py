5c0d1311f543f3e8951357626f8690a454dd0db8
jejjohnson <emanjohnson91@gmail.com>
2020-09-02 10:44:10 +0200

    added scott and silvermans factors.

--- src/kernels/utils/sigma.py
+++ src/kernels/utils/sigma.py
@@ -19,6 +19,57 @@ def estimate_sigma_median(X):
     return sigma
 
 
+def estimate_sigma_median_percent(X):
+    dists = pdist_squareform(X, X)
+
+    sigma = np.median(dists[np.nonzero(dists)])
+    return sigma
+
+
+def scotts_factor(X: np.ndarray) -> float:
+    """Scotts Method to estimate the length scale of the 
+    rbf kernel.
+
+        factor = n**(-1./(d+4))
+
+    Parameters
+    ----------
+    X : np.ndarry
+        Input array
+
+    Returns
+    -------
+    factor : float
+        the length scale estimated
+
+    """
+    n_samples, n_features = X.shape
+    return np.power(n_samples, -1 / (n_features + 4.0))
+
+
+def silvermans_factor(X: np.ndarray) -> float:
+    """Silvermans method used to estimate the length scale
+    of the rbf kernel.
+
+    factor = (n * (d + 2) / 4.)**(-1. / (d + 4)).
+
+    Parameters
+    ----------
+    X : np.ndarray,
+        Input array
+
+    Returns
+    -------
+    factor : float
+        the length scale estimated
+    """
+    n_samples, n_features = X.shape
+
+    base = (n_samples * (n_features + 2.0)) / 4.0
+
+    return np.power(base, -1 / (n_features + 4.0))
+
+
 # def estimate_sigma_k_median(X, percent=0.3):
 #     sigma = np.median(sqeuclidean_distance(X, X))
 #     return sigma
