0e89b14b451053f17602878fe3f8b2532cfdf394
jejjohnson <emanjohnson91@gmail.com>
2020-09-05 19:49:17 +0200

    Demo material theme.

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,34 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-html_theme = "sphinx_rtd_theme"
+# Required theme setup
+html_theme = "sphinx_material"
+
+# Set link name generated in the top bar.
+html_title = "Jaxkern"
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the navigation.
+    "nav_title": "jaxkern",
+    # Set you GA account ID to enable tracking
+    "google_analytics_account": "UA-XXXXX",
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    "base_url": "https://project.github.io/project",
+    # Set the color and the accent color
+    "color_primary": "black",
+    "color_accent": "black",
+    # Set the repo location to get a badge with stats
+    "repo_url": "https://github.com/ipl-uv/jaxkern/",
+    "repo_name": "jaxkern",
+    # Visible levels of the global TOC; -1 means unlimited
+    "globaltoc_depth": 3,
+    # If False, expand all TOC entries
+    "globaltoc_collapse": False,
+    # If True, show hidden TOC entries
+    "globaltoc_includehidden": False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
