0ecc911e803c474f16bf968bb3ba97fc03289977
jejjohnson <emanjohnson91@gmail.com>
2020-09-05 18:33:35 +0200

    Removed files.

--- jaxkern/similarity/__init__.py
+++
@@ -1,9 +0,0 @@
-import jax.numpy as np
-
-from jaxkern.kernels.dist import distmat, sqeuclidean_distance
-
-
-def distance_corr(X, sigma=1.0) -> float:
-    X = distmat(sqeuclidean_distance, X, X)
-    X = np.exp(-X / (2 * sigma ** 2))
-    return np.mean(X)
